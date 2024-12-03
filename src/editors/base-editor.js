cfa135146e7447329502babcbc70b282434f6cce
delphiki <delphiki@protonmail.com>
2024-12-03 21:36:53 +0100

    fix(base-editor): remove console.log()

--- src/editors/base-editor.js
+++ src/editors/base-editor.js
@@ -13,8 +13,6 @@ class BasePronoteCardEditor extends LitElement {
     }
 
     setConfig(config) {
-        console.log(this.hass);
-
         this._config = config;
         this.loadEntityPicker();
     }
