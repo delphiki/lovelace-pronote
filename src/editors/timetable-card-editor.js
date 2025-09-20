8cf778e40b4ff2970fb651b01c08d9747a309576
delphiki <delphiki@protonmail.com>
2025-09-20 23:40:17 +0200

    fix(timetable-card): add missing option to display teacher on timetable card editor

--- src/editors/timetable-card-editor.js
+++ src/editors/timetable-card-editor.js
@@ -18,6 +18,7 @@ class PronoteTimetableCardEditor extends BasePronoteCardEditor {
             ${this.buildSwitchField('Current week only', 'current_week_only', this._config.current_week_only, false)}
             ${this.buildNumberField('Max days', 'max_days', this._config.max_days, null, 1)}
             ${this.buildSwitchField('Display classroom', 'display_classroom', this._config.display_classroom, true)}
+            ${this.buildSwitchField('Display teacher', 'display_teacher', this._config.display_teacher, true)}
             ${this.buildSwitchField('Display day hours', 'display_day_hours', this._config.display_day_hours, true)}
             ${this.buildSwitchField('Display lunch break', 'display_lunch_break', this._config.display_lunch_break, true)}
             ${this.buildSwitchField('Dim ended lessons', 'dim_ended_lessons', this._config.dim_ended_lessons, true)}
